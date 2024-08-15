9103bf734d38a4d06f78405bfd422cdcb6f3aac3
shrav <shrav@Shravankumar>
2024-08-15 14:36:32 +0530

    Commit

--- src/qsp/Demo2.java
+++ src/qsp/Demo2.java
@@ -1,30 +1,27 @@
 package qsp;
 
-import org.testng.annotations.Test;
-
 public class Demo2 
 {
-     @Test
-     public void test2() 
-     {
-    	 for(int i=1;i<=3;i++)
- 		{
- 			for(int j=1;j<=i;j++)
- 			{
- 				System.out.print("*");
- 			}
- 			System.out.println();
- 		}
- 		for(int i =2;i>=1;i--)
- 		{
- 			for(int j=1;j<=i;j++) 
- 			{
- 				System.out.print("*");
- 				
- 			}
- 			System.out.println();
- 		}
-    	 
-     }
+	public static void main(String[] args) 
+	{
+		for(int i=1;i<=3;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(int i =2;i>=1;i--)
+		{
+			for(int j=1;j<=i;j++) 
+			{
+				System.out.print("*");
+				
+			}
+			System.out.println();
+		}
+		
+	}
 
 }
